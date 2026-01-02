d9fe113570f0e1d857000ff35ecf719d53dc3f1d
Manuel Schaich <rmschaich@outlook.com>
2026-01-02 19:45:49 +0000

    Set version for matlab toolbox

--- package.m
+++ package.m
@@ -1,13 +1,15 @@
-function package(tbname,basedir,outdir)
+function package(tbname,basedir,outdir,version)
 arguments
     tbname (1,1) string = "chebfun"
     basedir (1,1) string {mustBeFolder} = fileparts(mfilename("fullpath"));
     outdir (1,1) string {mustBeFolder} = fileparts(mfilename("fullpath"));
+    version (1,1) string = "1.0.0";
 end
 
 uuid = string(java.util.UUID.randomUUID);
 opts = matlab.addons.toolbox.ToolboxOptions(basedir,uuid);
 opts.ToolboxName = tbname;
+opts.ToolboxVersion = version;
 opts.OutputFile = fullfile(outdir,tbname+".mltbx");
 matlab.addons.toolbox.packageToolbox(opts);
 disp("Generated "+opts.OutputFile)

3bd4f31e55675d03fdb1297f2055457b18e6b390
Manuel Schaich <rmschaich@outlook.com>
2026-01-02 18:34:54 +0000

    Initial commit

---
+++ package.m
@@ -0,0 +1,14 @@
+function package(tbname,basedir,outdir)
+arguments
+    tbname (1,1) string = "chebfun"
+    basedir (1,1) string {mustBeFolder} = fileparts(mfilename("fullpath"));
+    outdir (1,1) string {mustBeFolder} = fileparts(mfilename("fullpath"));
+end
+
+uuid = string(java.util.UUID.randomUUID);
+opts = matlab.addons.toolbox.ToolboxOptions(basedir,uuid);
+opts.ToolboxName = tbname;
+opts.OutputFile = fullfile(outdir,tbname+".mltbx");
+matlab.addons.toolbox.packageToolbox(opts);
+
+end
